bf97af3901b12486797af86766786d3b1af0f742
trzcina <michal.chrzaszcz@wp.pl>
2022-02-04 12:11:22 +0100

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 This is a starter template for [Learn Next.js](https://nextjs.org/learn).
 
+This repo demonstrates snippet for website-nextjs and website-js usage
+
+To run:
+1. npm install
+2. create file .env.local with FLOTIQ_API_KEY=_key_
+3. import example data using flotiq-cli (data defined in .flotiq  dir)
+4. npm run dev
 
 ### TMP
 Command to build sdk
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import Layout, { siteTitle } from '../components/layout'
+import Layout, {siteTitle} from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import * as FlotiqUserApi from "../lib/flotiq-sdk/src";
 
@@ -45,41 +45,40 @@ const demoJS = async () => {
     });
 }
 
-export default function Home({ projectsData }) {
-
+export default function Home({projectsData}) {
     // run plain js demo
-   // demoJS();
+    // with result in console
+    // demoJS();
 
-  return (
-      <Layout home>
-        <Head>
-          <title>{siteTitle}</title>
-        </Head>
-        <section className={utilStyles.headingMd}>
-          <p>Lorem ipsum dolor sit amet</p>
-          <p>
-            (This is a sample website - you’ll be building a site like this on{' '}
-            <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-          </p>
-        </section>
+    return (
+        <Layout home>
+            <Head>
+                <title>{siteTitle}</title>
+            </Head>
+            <section className={utilStyles.headingMd}>
+                <p>Lorem ipsum dolor sit amet</p>
+                <p>
+                    (This is a sample website - you’ll be building a site like this on{' '}
+                    <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+                </p>
+            </section>
 
-          <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-              <h2 className={utilStyles.headingLg}>Blog</h2>
-              <ul className={utilStyles.list}>
-                  {projectsData.map(({ id, date, name }) => (
-                      <li className={utilStyles.listItem} key={id}>
-                          <Link href={`/projects/${id}`}>
-                              <a>{name}</a>
-                          </Link>
-                          <br />
-                          {id}
-                          <br />
-                          {date}
-                      </li>
-                  ))}
-              </ul>
-          </section>
-
-      </Layout>
-  )
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                    {projectsData.map(({id, date, name}) => (
+                        <li className={utilStyles.listItem} key={id}>
+                            <Link href={`/projects/${id}`}>
+                                <a>{name}</a>
+                            </Link>
+                            <br/>
+                            {id}
+                            <br/>
+                            {date}
+                        </li>
+                    ))}
+                </ul>
+            </section>
+        </Layout>
+    )
 }
