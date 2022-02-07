e82a44a9618d1a43ad6ae98492aa21d83352e424
trzcina <michal.chrzaszcz@wp.pl>
2022-02-07 11:11:42 +0100

    Update [id].js

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -7,7 +7,7 @@ FlotiqUserApi.ApiClient.instance.authentications['HeaderApiKeyAuth'].apiKey = pr
 
 export async function getStaticPaths() {
     const projects = await (new FlotiqUserApi.ContentProjectApi().listProject({
-        limit: 10000
+        limit: 1000
     }));
     return {
         paths: projects.data.map((project) => ({ params: { id: project.id } })),
